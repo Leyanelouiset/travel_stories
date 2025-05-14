b7c478ed264ee2c6bb19f1de056f5d62505f67d0
leyanelouiset <louisetleyane@gmail.com>
2025-05-14 15:25:15 +0200

    postmodel modification

--- backend/src/model/postModel.js
+++ backend/src/model/postModel.js
@@ -3,13 +3,85 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 class Post {
-  static async findAll() {
-    return await prisma.posts.findMany();
+  // Récupération de plusieurs posts avec pagination
+  static async findAll(limit = 10, skip = 0) {
+    try {
+      const posts = await prisma.posts.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          users: true,
+        },
+        take: limit,
+        skip: skip,
+      });
+
+      return posts;
+    } catch (error) {
+      console.error("Erreur lors de la récupération des posts:", error.message);
+      throw error;
+    }
   }
 
+  // Récupération d'un seul post par ID
   static async findOne(id) {
-    return await prisma.posts.findUnique({
-      where: { id: Number(id) },
-    });
+    try {
+      if (!id || isNaN(Number(id))) {
+        throw new Error("ID invalide");
+      }
+
+      const post = await prisma.posts.findUnique({
+        where: {
+          id: Number(id),
+        },
+        include: {
+          users: true,
+          comments: true,
+        },
+      });
+
+      if (!post) {
+        throw new Error("Post non trouvé");
+      }
+
+      return post;
+    } catch (error) {
+      console.error("Erreur lors de la récupération du post :", error.message);
+      throw error;
+    }
+  }
+
+
+static async create(data){
+  try{
+if(!data.text|| !data.user_id){
+  throw new Error ("Le texte du post et l'ID de l'utilisateur sont requis");
+      
+}
+  console.log("DATA REÇUE PAR PRISMA : ", data);
+      
+      // Création du post dans la base de données
+      const post = await prisma.posts.create({
+        data: {
+          text: data.text,
+          image: data.image, 
+          users: {
+            connect: {
+              id: Number(data.user_id), // Connexion à l'utilisateur existant
+            },
+          },
+        },
+        // Inclure les données de l'utilisateur dans le résultat
+        include: {
+          users: true,
+        },
+      });
+      
+      return post;
+    } catch (error) {
+      console.error("Erreur lors de la création du post :", error.message);
+      throw error;
+    }
   }
 }
